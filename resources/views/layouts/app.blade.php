452948a26ff7bab40531cba408a7c255d41d3b70
Dijisoftwares <dijisoftwares@gmail.com>
2024-12-04 14:10:19 +0300

    deployment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="m_gTdDMrO9ubkJXM-kQ0AN4YaqhUVEk72-kzgYlKNkI" />
     <meta property="og:site_name"
         content="Kwanza Images - Your premier photography and videography studio in Njoro Town, Kenya." />
     <meta property="og:title" content="Home - KWANZA IMAGES" />
